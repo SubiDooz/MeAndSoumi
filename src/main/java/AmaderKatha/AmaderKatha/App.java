222c43972d3d46eb56f98a2d2570614608fc7d1c
SubiDooz <geeksubhra@gmail.com>
2021-05-14 20:47:35 +0530

    first commit to amader katha

---
+++ src/main/java/AmaderKatha/AmaderKatha/App.java
@@ -0,0 +1,13 @@
+package AmaderKatha.AmaderKatha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
